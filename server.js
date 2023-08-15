f5a93ce2816caf58c4351a486e88825d54bbd5c0
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-08-15 15:51:09 +0700

    master

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+
+const db = require("./db/conn");
+app.listen(port, () => {
+  db.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port ${port}`);
+});
